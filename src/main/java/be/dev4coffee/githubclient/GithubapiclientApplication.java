dae42d96b5c8ac23f04baa4d7fe5be885ef1ca81
Jan Eckert <Jan.Eckert@rwth-aachen.de>
2022-04-02 16:43:48 +0200

    initial commit.

---
+++ src/main/java/be/dev4coffee/githubclient/GithubapiclientApplication.java
@@ -0,0 +1,13 @@
+package be.dev4coffee.githubclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubapiclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubapiclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/dev4coffee/githubclient/GithubapiclientApplicationTests.java
@@ -0,0 +1,13 @@
+package be.dev4coffee.githubclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubapiclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
